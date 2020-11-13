41f018b0f28ca1de114b80625d01e8c2fee8fdd3
Dario Milani <runner855@icloud.com>
2020-11-13 10:49:01 +0100

    adding passengers on board

--- _tests_/ship.test.js
+++ _tests_/ship.test.js
@@ -1,15 +1,37 @@
-const Ship = require("../src/Ship.js");
+const Ship = require("../src/ship.js");
 
 describe("constructor", () => {
     it("can be instantiated", () => {
-        expect( new Ship()).toBeInstanceOf(Object);
+        expect(new Ship("Dover")).toBeInstanceOf(Object);
     });
 
     it("has a starting port", () => {
-        const ship = new Ship("Dover");
+        const ship = {
+            startingPort:"Dover",
+        };
 
         expect(ship.startingPort).toBe("Dover");
     })
+
+    it(" has initial passengers 0", () => {
+        const ship = new Ship("Dover");
+
+        
+    
+        expect(ship.passengers).toEqual(0);
+    });
+});
+
+describe("onBoard", () => {
+it("increase the passengers by 1", () => {
+    const ship = new Ship("Dover");
+
+    ship.onBoard();
+
+    
+
+    expect(ship.passengers).toEqual(1);
+    });
 });
 
  
--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,13 @@
 function Ship(startingPort) {
     this.startingPort = startingPort;
+    this.passengers = 0;
 };
+
+Ship.prototype.onBoard = function() {
+     this.passengers += 1;
+    
+};
+    
 
 
 module.exports = Ship;
