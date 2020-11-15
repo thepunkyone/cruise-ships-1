0fe1cd07cdd8dc1e20c75ebd946185c5a1cc6846
Dario Milani <runner855@icloud.com>
2020-11-15 18:11:43 +0100

    created port.js and port test the make the new port have a name and allow the ship to dock to different ports

--- _tests_/ship.test.js
+++ _tests_/ship.test.js
@@ -1,37 +1,89 @@
-const Ship = require("../src/ship.js");
+const Ship = require("../src/Ship.js");
+const Port = require("../src/port.js")
 
 describe("constructor", () => {
+    let ship;
+    let port;
+
+    beforeEach(() => {
+        port = new Port("Dover");
+        ship = new Ship(port);
+    })
+
+
     it("can be instantiated", () => {
         expect(new Ship("Dover")).toBeInstanceOf(Object);
-    });
+    })
 
-    it("has a starting port", () => {
-        const ship = {
-            startingPort:"Dover",
-        };
+    it("has a current port", () => {
+        
 
-        expect(ship.startingPort).toBe("Dover");
+        expect(ship.currentPort).toBe(port);
     })
 
     it(" has initial passengers 0", () => {
-        const ship = new Ship("Dover");
 
-        
-    
         expect(ship.passengers).toEqual(0);
-    });
+    })
 });
 
 describe("onBoard", () => {
-it("increase the passengers by 1", () => {
-    const ship = new Ship("Dover");
+
+    let ship;
+
+    beforeEach(()=> {
+        ship = new Ship("Dover");
+    })
+    it("increase the passengers by 1", () => {
+    
 
     ship.onBoard();
 
-    
-
-    expect(ship.passengers).toEqual(1);
-    });
+        expect(ship.passengers).toEqual(1);
+    })
 });
+
+    describe("setSail", () => {
+
+        let port;
+        let ship;
+
+        beforeEach(() => {
+            port = new Port("Dover");
+            ship = new Ship(port);        
+        })
+
+        it("It can set sail", () => {
+            const port = new Port("Dover");
+            const ship = new Ship(port);
+
+            ship.setSail();
+
+            expect(ship.currentPort).toBeFalsy();
+        })
+    });
+
+    describe("dock", () => {
+
+        let port;
+        let ship;
+        let calais;
+
+        beforeEach(() => {
+            port = new Port("Dover");
+            ship = new Ship("dover");
+            calais = new Ship("Calais");
+        })
+
+        it("can dock at a different port", () => {
+            
+
+           
+            ship.dock(calais);
+
+            expect(ship.currentPort).toBe(calais);
+        })
+    });
+
 
  
--- src/ship.js
+++ src/ship.js
@@ -1,13 +1,48 @@
-function Ship(startingPort) {
-    this.startingPort = startingPort;
+const Port = require("./port");
+
+function Ship(currentPort) { 
+    this.currentPort = currentPort;
     this.passengers = 0;
+    this.name = name;
+    
 };
+    
+
+
+
+
 
 Ship.prototype.onBoard = function() {
      this.passengers += 1;
     
 };
+
+Ship.prototype.setSail = function() {
+
+    this.currentPort = 0;
+    
+};
+
+Ship.prototype.dock = function() {
+    
+    Ship.calais;
+
+};
+
+Ship.prototype.dock = function(port) {
+    this.currentPort = port;
+}
+
+
+
     
 
 
-module.exports = Ship;
+
+   
+
+    
+
+
+module.exports = Ship,
+                Port;
